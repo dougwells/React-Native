9bd694447e127b9b0dd9c04193a5714e537605ba
dougwells <dougwells@gmail.com>
2016-09-06 08:31:04 -0600

    ReactNative. Authentication.  Added secureTextEntry to hide password entry

--- authentication/src/components/signin.js
+++ authentication/src/components/signin.js
@@ -10,7 +10,12 @@ module.exports = React.createClass({
   render: function(){
     return(
       <View style={styles.container}>
-        <Text>Sign-in Page</Text>
+        <Text>Sign-in Below:</Text>
+        <Text style={styles.label}>Username:</Text>
+        <TextInput style={styles.input}></TextInput>
+        <Text style={styles.label}>Password:</Text>
+        <TextInput secureTextEntry={true} style={styles.input}></TextInput>
+
       </View>
     );
   }
@@ -21,5 +26,18 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
+  },
+  label: {
+    fontSize: 18
   }
 })
