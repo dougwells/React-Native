0063ee8caf7810d97e36e2eeadc30097e5f71d47
dougwells <dougwells@gmail.com>
2016-09-16 14:34:40 -0600

    ReactNative. EventExpert. Init Commit

---
+++ EventExpert/src/main.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  AsyncStorage
+} from 'react-native';
+
+module.exports = React.createClass({
+  getInitialState (){
+
+  }
+
+  render(){
+    return(
+      <View>
+        <Text>Welcome to Event Expert</Text>
+      </View>
+    )
+
+  }
+}
