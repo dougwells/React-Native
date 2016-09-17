010b01bebbc943407175a14044b41e61ecc8602d
dougwells <dougwells@gmail.com>
2016-09-17 10:00:41 -0600

    ReactNative. EventExpert.  Added scrollable ListView with dataSource

--- EventExpert/src/main.js
+++ EventExpert/src/main.js
@@ -2,19 +2,32 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
+  ListView,
   StyleSheet
 } from 'react-native';
-import API_KEY from '../secrets/api'
 
+import API_KEY from '../secrets/api'
 const ROOT_URL = 'https://www.eventbriteapi.com/v3/events/search/';
+const ds = new ListView.DataSource({rowHasChanged: (r1,r2)=>r1 !== r2});
 
 module.exports = React.createClass({
+  getInitialState(){
+    return{
+      dataSource: ds.cloneWithRows([
+        {
+          name: {text: 'Event 1'},
+          url: 'www.google.com'
+        }
+      ])
+    }
+  },
+
   componentDidMount(){
-    this.searchEvents('hackathon', 'San Francisco');
+    this.searchEvents('hackathon', 'New York City');
   },
 
   searchEvents(category, city){
-    const FETCH_URL = ROOT_URL + '?q=' + category + '&venue.city=' + city;
+    const FETCH_URL = `${ROOT_URL}?q=${category}&location.address=${city}/`;
     fetch(FETCH_URL, {
       method: 'GET',
       headers: {
@@ -26,11 +39,24 @@ module.exports = React.createClass({
       console.log('responseJSON', responseJSON);
     });
   },
+  renderRow(rowData) {
+    return (
+      <View>
+        <Text>{rowData.name.text}</Text>
+        <Text>{rowData.url}</Text>
+      </View>
+    )
+  },
 
   render(){
     return(
       <View style={styles.container}>
-        <Text style={styles.content}>Welcome to Event Expert</Text>
+        <Text style={styles.title}>Welcome to Event Expert</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData)=>this.renderRow(rowData)}
+          style={styles.list}
+          />
       </View>
     )
   }
@@ -42,7 +68,12 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  content: {
+  title: {
+    flex:1,
+    marginTop: 40,
     color: 'red'
+  },
+  list: {
+    flex:1
   }
 })
