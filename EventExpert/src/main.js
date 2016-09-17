1c89f16d420838600169d928bc65adb1578e3796
dougwells <dougwells@gmail.com>
2016-09-17 07:20:59 -0600

    ReactNative. EventExpert.  Fetch data from EventBrite API.

--- EventExpert/src/main.js
+++ EventExpert/src/main.js
@@ -2,24 +2,47 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  TextInput,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  AsyncStorage
+  StyleSheet
 } from 'react-native';
+import API_KEY from '../secrets/api'
+
+const ROOT_URL = 'https://www.eventbriteapi.com/v3/events/search/';
 
 module.exports = React.createClass({
-  getInitialState (){
+  componentDidMount(){
+    this.searchEvents('hackathon', 'San Francisco');
+  },
 
-  }
+  searchEvents(category, city){
+    const FETCH_URL = ROOT_URL + '?q=' + category + '&venue.city=' + city;
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers: {
+        'Authorization': API_KEY
+      }
+    })
+    .then((response)=> response.json())
+    .then((responseJSON)=>{
+      console.log('responseJSON', responseJSON);
+    });
+  },
 
   render(){
     return(
-      <View>
-        <Text>Welcome to Event Expert</Text>
+      <View style={styles.container}>
+        <Text style={styles.content}>Welcome to Event Expert</Text>
       </View>
     )
-
   }
-}
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  content: {
+    color: 'red'
+  }
+})
