81bd8e167e530ee192f259f1fad06c6fd34e0863
dougwells <dougwells@gmail.com>
2016-09-18 10:51:04 -0600

    ReactNative. EventExpert. Added form w/TextInput for custom event search

--- EventExpert/src/main.js
+++ EventExpert/src/main.js
@@ -3,7 +3,9 @@ import {
   View,
   Text,
   Image,
+  TextInput,
   ListView,
+  TouchableOpacity,
   StyleSheet
 } from 'react-native';
 
@@ -14,6 +16,8 @@ const ds = new ListView.DataSource({rowHasChanged: (r1,r2)=>r1 !== r2});
 module.exports = React.createClass({
   getInitialState(){
     return{
+      eventType: 'hackathon',
+      city: 'San Francisco',
       dataSource: ds.cloneWithRows([
         {
           name: {text: 'Event 1'},
@@ -24,7 +28,7 @@ module.exports = React.createClass({
   },
 
   componentDidMount(){
-    this.searchEvents('hackathon', 'San Francisco');
+    this.searchEvents(this.state.eventType, this.state.city);
   },
 
   searchEvents(category, city){
@@ -65,10 +69,43 @@ module.exports = React.createClass({
     )
   },
 
+  renderDefaultSubTitle(){
+    // if(this.state.eventType === 'hackathon' && this.state.city === 'San Francisco'){
+      return (
+        <Text style={styles.subTitle}>
+          (Results for "{this.state.eventType}" in {this.state.city})
+        </Text>
+      )
+    // }
+  },
+
   render(){
     return(
       <View style={styles.container}>
-        <Text style={styles.title}>Welcome to Event Expert</Text>
+        <View style={styles.heading}>
+          <Text style={styles.title}>
+            Welcome to Event Expert
+          </Text>
+        </View>
+        <View style={styles.form}>
+          <TextInput
+                style={styles.input}
+                placeholder="What type of event ...?"
+                onChangeText={(text)=>this.setState({eventType: text})}
+          />
+          <TextInput
+                style={styles.input}
+                placeholder="In what city?"
+                onChangeText={(text)=>this.setState({city: text})}
+          />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={()=>{this.searchEvents(this.state.eventType, this.state.city)}}
+          >
+          <Text style={styles.button}>Search</Text>
+        </TouchableOpacity>
+        </View>
+        {this.renderDefaultSubTitle()}
         <ListView
           dataSource={this.state.dataSource}
           renderRow={(rowData)=>this.renderRow(rowData)}
@@ -82,16 +119,50 @@ module.exports = React.createClass({
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex:1
+  },
+  heading: {
+    flex: 1,
+    marginTop: 40
   },
   title: {
-    flex:1,
-    marginTop: 40,
     textAlign: 'center',
+    fontSize: 20,
     color: 'red'
   },
+
+  form: {
+    flex: 3
+  },
+  input: {
+    flex: 1,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    flex: 1
+  },
+  button: {
+    flex: 1,
+    borderColor: '#0000FF',
+    borderWidth: 1,
+    borderRadius: 5,
+    textAlign: 'center',
+    color: '#0000FF',
+    margin: 5,
+    padding: 5
+  },
+  subTitle: {
+    marginTop: 10,
+    textAlign: 'center',
+    color: "green"
+  },
+
   list: {
-    flex:8
+    flex:10
   },
   row: {
     flex: 1,
@@ -110,4 +181,6 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     borderWidth: 1
   }
+
+
 })
