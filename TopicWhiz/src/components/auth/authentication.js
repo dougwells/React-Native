6c3186d4a87ca593eef19f79123f63e753a80507
dougwells <dougwells@gmail.com>
2016-09-20 07:25:43 -0600

    ReactNative. TopicWhiz. Firebase initialization.

---
+++ TopicWhiz/src/components/auth/authentication.js
@@ -0,0 +1,16 @@
+import apiKey from './secrets/secret';
+
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: apiKey,
+    authDomain: "react-native-topicwhiz.firebaseapp.com",
+    databaseURL: "https://react-native-topicwhiz.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "1032265988190"
+  };
+
+
+  module.exports = firebase.initializeApp(config);
+
+  // export const firebase = firebase.initializeApp(config);
