82f94000a3a4c45c5a1080a4c19a7aa4ec4f425a
dougwells <dougwells@gmail.com>
2016-09-22 08:22:56 -0600

    ReactNative. TopicWhiz. More routing.Topics page back to signup page.

--- TopicWhiz/src/components/topics.js
+++ TopicWhiz/src/components/topics.js
@@ -2,16 +2,30 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  TextInput
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 import styles from '../styles';
 
 module.exports = React.createClass({
+  signOut(){
+    console.log("signOut invoked");
+    this.props.navigator.pop()
+  },
+
   render(){
     return (
       <View style={styles.container}>
         <Text>Topics Page</Text>
+        <TouchableOpacity>
+          <Text
+            style={styles.link}
+            onPress={this.signOut}
+          >
+            Return to signIn Page
+          </Text>
+        </TouchableOpacity>
       </View>
     )
 
