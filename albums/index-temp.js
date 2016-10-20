0f04690dab2814efc2e7ef1bd823936e1adbc983
dougwells <dougwells@gmail.com>
2016-10-20 07:21:14 -0600

    ReactNative. Albums. Rebuilding index.ios.js to understand core code

---
+++ albums/index-temp.js
@@ -0,0 +1,15 @@
+//Building index.ios.js
+
+// Import Library to create a Component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component
+const App = () => {
+  return(
+    <Text>Some Text</Text>
+  );
+};
+
+
+// Render it to iOS Device
